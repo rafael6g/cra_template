81ac1cc019ca865f3d07c14795adf3f69d8946af
Rafael Fernandes <rafael.londrina@gmail.com>
2020-06-13 14:48:54 -0300

    first commit

---
+++ src/pages/Signin/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Signin() {
+  return <h1>Sign in</h1>;
+}
+
+export default Signin;
---
+++ src/pages/Signup/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Signup() {
+  return <h1>sign Up</h1>;
+}
+
+export default Signup;
